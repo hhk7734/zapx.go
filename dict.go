97b5036007546c3b0e432aa6606bbe1056873772
Hyeonki Hong <hhk7734@gmail.com>
2023-07-05 00:59:56 +0900

    docs: add comment about Dict

--- dict.go
+++ dict.go
@@ -5,6 +5,16 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// Dict constructs a Field with key and fs. It is useful for adding a nested
+// json object to a log message.
+//
+// Example:
+//
+//	log.Info("test",
+//		zapx.Dict("foo",
+//			zap.String("bar", "baz")))
+//
+//	{"msg": "test", "foo":{"bar": "baz"}}
 func Dict(key string, fs ...zap.Field) zap.Field {
 	return zap.Object(key, zapcore.ObjectMarshalerFunc(func(enc zapcore.ObjectEncoder) error {
 		for _, f := range fs {
