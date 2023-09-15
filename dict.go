f5ff015def65f726a313b399ff6d927e26222461
Hyeonki Hong <hhk7734@gmail.com>
2023-09-15 15:13:37 +0900

    feat: use zap.Dict

--- dict.go
+++ dict.go
@@ -2,24 +2,7 @@ package zapx
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-// Dict constructs a Field with key and fs. It is useful for adding a nested
-// json object to a log message.
-//
-// Example:
-//
-//	log.Info("test",
-//		zapx.Dict("foo",
-//			zap.String("bar", "baz")))
-//
-//	{"msg": "test", "foo":{"bar": "baz"}}
-func Dict(key string, fs ...zap.Field) zap.Field {
-	return zap.Object(key, zapcore.ObjectMarshalerFunc(func(enc zapcore.ObjectEncoder) error {
-		for _, f := range fs {
-			f.AddTo(enc)
-		}
-		return nil
-	}))
-}
+// Deprecated: use zap.Dict instead. (go.uber.org/zap v1.26.0)
+var Dict = zap.Dict
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/hhk7734/zapx.go
 go 1.20
 
 require (
-	go.uber.org/zap v1.24.0
+	go.uber.org/zap v1.26.0
 	gorm.io/gorm v1.25.2
 )
 
@@ -11,5 +11,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 )
